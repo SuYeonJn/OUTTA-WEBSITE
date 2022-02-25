368b86007aed1cab9e7195892edf63a61a5cf025
SuYeonJn <99309345+SuYeonJn@users.noreply.github.com>
2022-02-26 02:19:41 +0900

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,22 @@
+/*window.onscroll = function() {myFunction()}; 
+
+function myFunction() {
+    if (400 < document.body.scrollTop || 400< document.documentElement.scrollTop) {
+    document.getElementById("pro").src = "프로그램 일러.png";
+    } else {
+        document.getElementById("pro").src = "프로그램 일러-1.png";
+
+    }
+    if(document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+    document.getElementById("book").src = "책 일러.png";
+    } else{
+        document.getElementById("book").src = "책 일러-1.png";
+    }
+    if(document.body.scrollTop > 1600 || document.documentElement.scrollTop > 1600){
+    document.getElementById("file").src = "자료실 일러.png";
+    } else{
+        document.getElementById("file").src = "자료실 일러-1.png";
+    }
+}*/
+
+
